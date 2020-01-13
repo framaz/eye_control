a7c8240916cd37b14d3f9212aab3126f54dd5bce
framaz <framaz@yandex.ru>
2020-01-13 14:44:35 +0500

    data enhancer for head pos axis
    it solves pnp itself as it is not complex operation

--- watcher/data_enhancer.py
+++ watcher/data_enhancer.py
@@ -3,6 +3,7 @@ from math import sqrt
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 import head_tracker
+import from_internet_or_for_from_internet.PNP_solver as pnp_solver
 
 class AbsDataEnhancer:
     def __init__(self, **kwargs):
@@ -11,7 +12,9 @@ class AbsDataEnhancer:
     def process(self, pic, np_points):
         if type(self) == AbsDataEnhancer:
             raise NotImplementedError("AbsDataEnhancer process call")
+
         if not isinstance(pic, Image.Image):
+            pic = pic.astype(dtype=np.uint8)
             pic = Image.fromarray(pic)
         pic = pic.convert('RGB')
         output = {}
@@ -27,7 +30,8 @@ class PointDataEnhancer(AbsDataEnhancer):
         pic, output = super().process(pic, np_points)
         drawer = ImageDraw.Draw(pic)
         for [x, y] in np_points:
-            drawer.ellipse([x - self.point_radius, y - self.point_radius, x + self.point_radius, y + self.point_radius], fill=0)
+            drawer.ellipse([x - self.point_radius, y - self.point_radius, x + self.point_radius, y + self.point_radius],
+                           fill=0)
         return pic, output
 
 
@@ -41,17 +45,44 @@ class WidthHeightDataEnhancer(PointDataEnhancer):
         pic, output = super().process(pic, np_points)
         drawer = ImageDraw.Draw(pic)
         font = ImageFont.truetype("arial.ttf", size=self.text_size)
-        for first, second in head_tracker.width_metrics+head_tracker.heigth_metrics:
+        for first, second in head_tracker.width_metrics + head_tracker.heigth_metrics:
             first = np_points[first]
             second = np_points[second]
             drawer.line([*first, *second], width=self.line_width)
             [text_x, text_y] = second
             text_x += 2
-            val = sqrt((first[0]-second[0])**2 + (first[1]-second[1])**2)
+            val = sqrt((first[0] - second[0]) ** 2 + (first[1] - second[1]) ** 2)
             drawer.text([text_x, text_y], str(int(val)), font=font, fill=(255, 0, 0))
         return pic, output
+
 
 if __name__ == "__main__":
     lel = WidthHeightDataEnhancer(kek=20)
     ImageFont.truetype("arial.ttf", size=8)
     lel = lel
+
+
+class LeftTopCornerTextWriteDataEnhancer(AbsDataEnhancer):
+    def __init__(self, text_size=9, **kwargs):
+        self.text_size = text_size
+        super().__init__(**kwargs)
+
+    def process(self, pic: Image.Image, np_points: np.ndarray, obj_to_out="kek"):
+        text = str(obj_to_out)
+        pic, output = super().process(pic, np_points)
+        drawer = ImageDraw.Draw(pic)
+        font = ImageFont.truetype("arial.ttf", size=self.text_size)
+        drawer.multiline_text([0, 0], text, fill=(255, 0, 0), font=font)
+        return pic, output
+
+
+class HeadPositionAxisDataEnhancer(AbsDataEnhancer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def process(self, pic, np_points):
+        pic, output = super().process(pic, np_points)
+        solver = pnp_solver.PoseEstimator((1080, 1920))
+        rotation, translation = solver.solve_pose(np_points)
+        pic = solver.draw_axes(pic, rotation, translation)
+        return pic, output
